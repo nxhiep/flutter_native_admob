ec2a351c335ed0b6292e724ddc1683baa9eb4086
Hiep Nguyen <nxhiep.1996@gmail.com>
2020-08-07 10:24:35 +0700

    minor change

--- ios/Classes/NativeAdmobController.swift
+++ ios/Classes/NativeAdmobController.swift
@@ -116,14 +116,14 @@ extension NativeAdmobController: GADUnifiedNativeAdLoaderDelegate {
         self.nativeAd = nativeAd
     }
 
-    func nativeAdDidRecordImpression(_ nativeAd: GADUnifiedNativeAd) {
-        print("nativeAdDidRecordImpression 1111111")
-    }
+    // func nativeAdDidRecordImpression(_ nativeAd: GADUnifiedNativeAd) {
+    //     print("nativeAdDidRecordImpression 1111111")
+    // }
 
-    func nativeAdDidRecordClick(_ nativeAd: GADUnifiedNativeAd) {
-        print("nativeAdDidRecordClick 111111")
-        channel.invokeMethod(LoadState.onAdClicked.rawValue, arguments: nil)
-    }
+    // func nativeAdDidRecordClick(_ nativeAd: GADUnifiedNativeAd) {
+    //     print("nativeAdDidRecordClick 111111")
+    //     channel.invokeMethod(LoadState.onAdClicked.rawValue, arguments: nil)
+    // }
 }
 
 extension NativeAdmobController: GADUnifiedNativeAdDelegate {
